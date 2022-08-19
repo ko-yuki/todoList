ca70964b3c6584b197373823c827d6766a36e952
xuezhonglin <xzl0318@126.com>
2022-08-19 10:52:04 +0800

    简单的todolist

---
+++ README.md
@@ -0,0 +1,19 @@
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
+
+### 简单的todoList
+
+使用 `umi + ts + antd + localStorage` 实现一个简单的 `todoList` ，可以添加和撤销任务。
---
+++ src/pages/index.tsx
@@ -0,0 +1,141 @@
+import styles from './index.less';
+import { Form, Input, Checkbox, Card, Button, Tooltip, message, Empty } from 'antd';
+import { useState, useEffect } from 'react';
+
+interface Task {
+  title: string;
+  key: number;
+  remark?: string;
+}
+
+interface State {
+  todoList: Task[];
+  doneList: Task[];
+}
+
+message.config({
+  top: 100,
+  duration: 2,
+  maxCount: 2,
+});
+
+export default function IndexPage() {
+
+  const [state, setState] = useState<State>({ todoList: [], doneList: [] });
+  const addForm = Form.useForm();
+
+  // 初始化从localStorage中获取任务列表
+  useEffect(() => {
+    const todoList: Task[] = JSON.parse(localStorage.getItem('todo')!) || [];
+    const doneList: Task[] = JSON.parse(localStorage.getItem('done')!) || [];
+    setState({ todoList, doneList });
+  }, []);
+
+  // 监听任务列表的变化并保存在本地
+  useEffect(() => {
+    localStorage.setItem('todo', JSON.stringify(state.todoList));
+    localStorage.setItem('done', JSON.stringify(state.doneList));
+  }, [state.todoList, state.doneList]);
+
+  // 添加任务
+  function handelAdd(value: Task) {
+    const todoList = [...state.todoList];
+    const key = (new Date()).getTime();
+    todoList.push({ ...value, key });
+    setState({ ...state, todoList });
+    message.success('添加成功！');
+    addForm[0].resetFields();
+  }
+
+  // 完成任务
+  function handelFinish(value: { todoList: number[] }) {
+    const { todoList, doneList } = state;
+    // 已完成
+    const done = todoList.filter(t => value.todoList.includes(t.key));
+    const newDoneList = [...doneList];
+    newDoneList.push(...done);
+    // 剩余
+    const has = todoList.filter(t => !value.todoList.includes(t.key));
+
+    setState({ todoList: has, doneList: newDoneList });
+  }
+
+  // 撤销已完成
+  function handelRevoke(value: { doneList: number[] }) {
+    const { todoList, doneList } = state;
+    // 已撤销
+    const todo = doneList.filter(t => value.doneList.includes(t.key));
+    const newTodoList = [...todoList];
+    newTodoList.push(...todo);
+    // 剩余
+    const has = doneList.filter(t => !value.doneList.includes(t.key));
+
+    setState({ todoList: newTodoList, doneList: has });
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.taskListBox}>
+        <div className={styles.taskBox}>
+          <Card bordered title='待完成任务'>
+            {
+              state.todoList.length ?
+                <Form onFinish={handelFinish} initialValues={{ todoList: [...state.todoList] }}>
+                  <Form.Item>
+                    <Button type='primary' htmlType='submit'>完成</Button>
+                  </Form.Item>
+                  <Form.Item name='todoList' className={styles.todoListBox}>
+                    <Checkbox.Group>
+                      {state.todoList.map(t => (
+                        <div key={t.key} className={styles.todoItem}>
+                          {t.remark ?
+                            <Tooltip title={t.remark}>
+                              <Checkbox value={t.key}>{t.title}</Checkbox>
+                            </Tooltip> :
+                            <Checkbox value={t.key}>{t.title}</Checkbox>}
+                        </div>
+                      ))}
+                    </Checkbox.Group>
+                  </Form.Item>
+                </Form> :
+                <Empty description='全部完成啦' />
+            }
+          </Card>
+          <Card bordered title='已完成任务'>
+            {
+              state.doneList.length ?
+                <Form onFinish={handelRevoke} initialValues={{ doneList: [...state.doneList] }}>
+                  <Form.Item>
+                    <Button type='primary' htmlType='submit'>撤销</Button>
+                  </Form.Item>
+                  <Form.Item name='doneList' className={styles.doneListBox}>
+                    <Checkbox.Group>
+                      {state.doneList.map(t => (
+                        <div key={t.key} className={styles.doneItem}>
+                          <Checkbox value={t.key}>{t.title}</Checkbox>
+                        </div>
+                      ))}
+                    </Checkbox.Group>
+                  </Form.Item>
+                </Form> :
+                <Empty description='空空如也' />
+            }
+          </Card>
+        </div>
+      </div>
+      <Card title='请添加任务' className={styles.addTask}>
+        <Form onFinish={handelAdd} initialValues={{ title: '', remark: '', checked: false }} form={addForm[0]}>
+          <Form.Item name='title' label='任务' rules={[{ required: true, message: '请输入任务标题！' }]}>
+            <Input placeholder='请输入任务标题' />
+          </Form.Item>
+          <Form.Item name='remark' label='备注'>
+            <Input.TextArea placeholder='请输入备注' />
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary' htmlType='submit'>添加</Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+}
